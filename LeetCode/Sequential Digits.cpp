2b64872ce6cd770f4a015593ed2fa98b463ac349
Jlindovsky <j.lindovsky@icloud.com>
2024-02-02 18:39:42 +0100

    leetCode #2

---
+++ LeetCode/Sequential Digits.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution
+{
+public:
+    std::vector<int> sequentialDigits(int low, int high)
+    {
+        // sorted array
+        std::vector<int> arr{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        std::vector<int> result{};
+        int temp = 0;
+        // loop increasing number of digits in sequential number
+        for (int i = 1; i < 9; i++)
+        {
+            // loop thru array with specified number of digits
+            for (int j = 0; j < (9 - i); j++)
+            {
+                int l = j;
+                int k = i;
+                // creating  number from digits
+                while (k >= 0)
+                {
+                    temp = temp * 10 + arr[l];
+                    k--;
+                    l++;
+                }
+                // controling if number is in asked interval
+                if ((temp >= low) && (temp <= high))
+                {
+                    result.push_back(temp);
+                }
+                temp = 0;
+            }
+        }
+        return result;
+    }
+};
