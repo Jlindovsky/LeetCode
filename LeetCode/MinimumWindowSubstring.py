1df216bd71f495758ffa651fe52e8d870f9755e6
Jlindovsky <j.lindovsky@icloud.com>
2024-02-04 03:08:32 +0100

    LeetCode #4

---
+++ LeetCode/MinimumWindowSubstring.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        finalSub = ""
+        
+        return finalSub
+
+def main():
+
+    solution = Solution()
+    input_s = "aab"
+    input_t = "ab"
+    result = solution.minWindow(input_s, input_t)
+    print("Minimum window:", result)
+
+
+if __name__ == "__main__":
+    main()
